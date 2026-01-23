5f8b02b49654c1fe44c05da5b844810de0b7f391
Anthoniicore <167034505+Anthoniicore@users.noreply.github.com>
2026-01-23 07:38:56 -0700

    Update fp_motion_blur.cpp

--- src/chimera/fix/interpolate/fp_motion_blur.cpp
+++ src/chimera/fix/interpolate/fp_motion_blur.cpp
@@ -7,8 +7,8 @@
 
 namespace Chimera {
 
-    static float last_pitch = 0.0f; // x
-    static float last_yaw   = 0.0f; // y
+    static float last_pitch = 0.0f;
+    static float last_yaw   = 0.0f;
     static bool  has_last   = false;
 
     constexpr float BLUR_STRENGTH = 0.35f;
@@ -18,7 +18,7 @@ namespace Chimera {
             return;
         }
 
-        auto &cam = get_camera_data();
+        auto &cam = camera_data;
 
         float pitch = cam.orientation[0].x;
         float yaw   = cam.orientation[0].y;
@@ -42,7 +42,7 @@ namespace Chimera {
             return;
         }
 
-        auto &cam = get_camera_data();
+        auto &cam = camera_data;
 
         cam.orientation[0].x = last_pitch;
         cam.orientation[0].y = last_yaw;
@@ -60,4 +60,3 @@ namespace Chimera {
     }
 
 }
-
